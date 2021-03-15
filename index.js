9babfa752a4bb6ab71f3d916ad67ab9133c38a2b
seoyun.son <sallyluckyyun@gmail.com>
2021-03-15 11:14:12 +0900

    ADD: Server/Client connection success

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const http = require("http").Server(app);
 const io = require("socket.io").listen(http);
 const port = process.env.PORT || 3000;
 
+////// server
+
 // var jsdom = require("jsdom");
 // var JSDOM = jsdom.JSDOM;
 // global.document = new JSDOM(``).window.document;
@@ -19,8 +21,8 @@ io.on("connection", (socket) => {
     console.log("user disconnected");
   });
   socket.on("message", (msg) => {
-    console.log(msg);
-    io.emit("message", msg);
+    console.log("server socket msg" + msg);
+    io.sockets.emit("message1", msg);
   });
   // socket.on("chat message", function (msg) {
   //   console.log("message: " + msg);
